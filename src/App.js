cf5d57053064be5b163fc24799680bfbd6b0eb0f
Leandro Guaraldi <leandro_319@yahoo.com.br>
2021-02-11 15:05:32 -0300

    estrutura inicial: router, styled, materialUi

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import Routes from './routes/index'
+import {BrowserRouter} from 'react-router-dom'
+
+
+const App = () => {
+  return(
+    <>
+    <BrowserRouter>
+      <Routes/>
+    </BrowserRouter>
+  </>
+  )
+}
+
+export default App;
