3ee7cc0a5b4cf9f81c1690e54066fee7976ee9f4
Leandro Guaraldi <leandro_319@yahoo.com.br>
2021-02-15 02:11:31 -0300

    v1.0.0

--- src/pages/RepoDetail/index.js
+++ src/pages/RepoDetail/index.js
@@ -3,15 +3,19 @@ import { useRouteMatch } from "react-router-dom";
 import moment from "moment";
 
 import api from "../../services/apiGit";
-
-import {StyledContainerRepo, ContentInfoDev, InfoRepository} from './styles'
 import Header from "../../components/Header";
 
+import { StyledContainerRepo, ContentInfoDev, InfoRepository } from "./styles";
+import { Typography } from "@material-ui/core";
+
+import StarIcon from "@material-ui/icons/Star";
+import SupervisorAccountIcon from '@material-ui/icons/SupervisorAccount';
+import CodeIcon from '@material-ui/icons/Code';
+import SwapHorizontalCircleIcon from "@material-ui/icons/SwapHorizontalCircle";
 
 const RepoDetail = () => {
   const { params } = useRouteMatch();
   const [datailsRepo, setDetailsRepo] = useState({});
-
 
   useEffect(() => {
     api.get(`repos/${params.detail}`).then((res) => {
@@ -24,28 +28,59 @@ const RepoDetail = () => {
 
   return (
     <>
-    <StyledContainerRepo>
-      <Header/>
-      <ContentInfoDev>
-        <h1>{datailsRepo.full_name}</h1>
-      </ContentInfoDev>
-      <InfoRepository>
-        
-        <div>
-          <p>Link Externo: {datailsRepo.url}</p>
+      <StyledContainerRepo>
+        <Header />
+        <ContentInfoDev>
+          <Typography color="primary" variant="h1">
+            Detalhes do Repositório
+          </Typography>
+          <Typography color="primary" variant="h1">
+          {datailsRepo.full_name}
+          </Typography>
+          <div className="static-repo">
+            <div className="repo-statis">
+              <StarIcon/>
+              <p>{datailsRepo.stargazers_count}</p>
+            </div>
+            <div className="repo-statis">
+              <SupervisorAccountIcon />
+              <p>{datailsRepo.subscribers_count}</p>
+            </div>
+            <div className="repo-statis">
+              <CodeIcon />
+              <p>Linguagens: {datailsRepo.language}</p>
+            </div>
+            <div className="repo-statis">
+              <SwapHorizontalCircleIcon/>
+              <p>Forks: {datailsRepo.forks}</p>
+            </div>
 
-        <p>data criação: {dateFormater(datailsRepo.created_at)}</p>
-          <p>ultima atualização: {dateFormater(datailsRepo.updated_at)}</p>
-          <p>ultimo push:{dateFormater(datailsRepo.pushed_at)}</p>
-        </div>
-          <p>Estrelas: {datailsRepo.stargazers_count}</p>
-          <p>Linguagens:{datailsRepo.language}</p>
-          <p>Forks:{datailsRepo.forks}</p>
-          <p>subscribers:{datailsRepo.subscribers_count}</p>
-
-      </InfoRepository>
-    </StyledContainerRepo>
-      </>
+          </div>
+          <div className="dates-repo">
+          <p>
+            <strong>Nome Completo: </strong> {datailsRepo.full_name}
+          </p>
+          <p>
+            <strong>Data Criação: </strong> {dateFormater(datailsRepo.created_at)}
+          </p>
+          <p>
+            <strong>Ultima atualização: </strong>{" "}
+            {dateFormater(datailsRepo.updated_at)}
+          </p>
+          <p>
+            <strong>Ultimo Push: </strong> {dateFormater(datailsRepo.pushed_at)}
+          </p>
+          </div>
+          <div className="repo-statis-link">          
+          <p>
+            <strong>Link Externo:</strong> {datailsRepo.svn_url}
+          </p>
+            <a href={datailsRepo.svn_url}>Acessar Repositório</a>
+          </div>
+        </ContentInfoDev>
+        <InfoRepository></InfoRepository>
+      </StyledContainerRepo>
+    </>
   );
 };
 
--- src/pages/RepoDetail/styles.js
+++ src/pages/RepoDetail/styles.js
@@ -11,9 +11,73 @@ export const ContentInfoDev = styled.div`
   align-items: center;
   justify-content: center;
 
-  img{
-    width: 200px;
+  h1{
+    font-size: 3rem;
   }
+  strong{
+    color: #7e84fc;
+  }
+  .static-repo{
+    width: 100%;
+    display: flex;
+    flex-direction: row;
+    justify-content: center;
+    @media(max-width: 400px){
+      flex-direction: column;
+      justify-content: flex-start;
+    }
+  }
+
+  .dates-repo{
+    width: 100%;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    flex-direction: column;
+    @media(max-width: 400px){
+      align-items: flex-start;
+    }
+  }
+
+  .repo-statis{
+    display: flex;
+    flex-direction: row;
+    justify-content: center;
+    align-items: center;
+    padding: 20px;
+    svg{
+      color: #7e84fc;
+    }
+    @media(max-width: 400px){
+      justify-content: flex-start;
+      padding: 5px;
+    }
+  }
+
+  .repo-statis-link{
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+
+    @media(max-width: 400px){
+      justify-content: flex-start;
+      padding: 5px;
+    }
+    a{
+    background-color: #7e84fc;
+    text-decoration: none;
+    padding: 15px;
+    border-radius: 15px;
+    color: #fff;
+    transition: all .2s ease-in-out;
+    margin-bottom: 30px;
+    &:hover{
+      background-color: #babdff;
+      color:  #7e84fc;
+    }
+    }
+      }
   `
   export const InfoRepository = styled.div`
 
